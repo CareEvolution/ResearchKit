a2938aa3ab9e380f770157456dd08a02e89799a1
Scott Guelich <scott@careevolution.com>
2016-05-24 11:19:56 -0700

    Make ORKCustomStepView.h public and ensure it's not stripped in Release.
    RK's podspec exports all private headers as public, so this header was
    public when we created our custom step for optional dates.
    
    Now that we use Carthage, it relies on the xcodeproj file instead, and
    the header being private causes linker errors when building for Release.
    This appears to be the only real option for customizing steps, so we
    are modifying our fork and will submit an issue/PR with RK upstream.

--- ResearchKit/Common/ORKCustomStepView.h
+++ ResearchKit/Common/ORKCustomStepView.h
@@ -77,6 +77,7 @@ ORK_CLASS_AVAILABLE
 
 @class ORKSurveyAnswerCell;
 
+ORK_CLASS_AVAILABLE
 @interface ORKQuestionStepCellHolderView : ORKQuestionStepCustomView
 
 @property (nonatomic, strong, nullable) ORKSurveyAnswerCell *cell;
