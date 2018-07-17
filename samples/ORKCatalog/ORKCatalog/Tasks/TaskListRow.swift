f18475e4fef14376fba824dfeae82fbc2b5f9382
Eric Schramm <schramm@careevolution.com>
2018-07-17 07:44:47 -0500

    implement requires scroll to bottom to agree
    # Conflicts:
    #	ResearchKit/Consent/ORKConsentReviewController.m

--- samples/ORKCatalog/ORKCatalog/Tasks/TaskListRow.swift
+++ samples/ORKCatalog/ORKCatalog/Tasks/TaskListRow.swift
@@ -1177,7 +1177,7 @@ enum TaskListRow: Int, CustomStringConvertible {
         */
         let signature = consentDocument.signatures!.first
         
-        let reviewConsentStep = ORKConsentReviewStep(identifier: String(describing:Identifier.consentReviewStep), signature: signature, in: consentDocument)
+        let reviewConsentStep = ORKConsentReviewStep(identifier: String(describing:Identifier.consentReviewStep), signature: signature, in: consentDocument, requiresScrollToBottom: true);
         
         // In a real application, you would supply your own localized text.
         reviewConsentStep.text = loremIpsumText
