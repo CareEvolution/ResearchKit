b4da4355290a7a8df0a1f1a5596e2176c90bdaa7
Mike Mertsock <mertsock@careevolution.com>
2022-12-21 16:33:18 -0500

    Load remote URL content for web view steps. Refactor preloader.

--- ORK1Kit/ORK1Kit/Common/ORK1WebViewStepViewController.h
+++ ORK1Kit/ORK1Kit/Common/ORK1WebViewStepViewController.h
@@ -35,11 +35,26 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@class ORK1WebViewStep;
+
 ORK1_CLASS_AVAILABLE
+/// Caches at most exactly one instance of a WKWebView.
+///
+/// Intended for offscreen loading of web content before there is a view controller to contain the web view, so that the content is ready to immediately render once it is time to display on-screen.
 @interface ORK1WebViewPreloader : NSObject
+
+/// A singleton instance.
 + (instancetype)shared;
-- (void)preload:(NSString *)htmlString baseURL:(nullable NSURL *)baseURL forKey:(NSString *)key;
-- (WKWebView *)webViewForKey:(NSString *)key baseURL:(nullable NSURL *)baseURL;
+
+@property (nonatomic) NSURLRequestCachePolicy remoteURLCachePolicy;
+@property (nonatomic) NSTimeInterval remoteURLTimeoutInterval;
+
+/// Creates and stores a web view, and immediately begins loading content in the view as specified by the `webViewStep`. Replaces any previously stored web view.
+/// - Parameters:
+///   - webViewStep: Defines the content to load in the web view.
+///   - key: A unique identifier for the web view.
+- (void)preload:(ORK1WebViewStep *)webViewStep forKey:(NSString *)key;
+
 @end
 
 /**
