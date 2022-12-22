77b62cb7b38b31962aa83616976cbbc5c28d6e16
Mike Mertsock <mertsock@careevolution.com>
2022-12-22 09:18:15 -0500

    ResearchKit v2 WebViewStep changes

--- ResearchKit/Common/ORKWebViewStepViewController.h
+++ ResearchKit/Common/ORKWebViewStepViewController.h
@@ -34,11 +34,26 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@class ORKWebViewStep;
+
 ORK_CLASS_AVAILABLE
+/// Caches at most exactly one instance of a WKWebView.
+///
+/// Intended for offscreen loading of web content before there is a view controller to contain the web view, so that the content is ready to immediately render once it is time to display on-screen.
 @interface ORKWebViewPreloader : NSObject
+
+/// A singleton instance.
 + (instancetype)shared;
-- (void)preload:(NSString *)htmlString baseURL:(nullable NSURL *)baseURL forKey:(NSString *)key;
-- (WKWebView *)webViewForKey:(NSString *)key baseURL:(nullable NSURL *)baseURL;
+
+@property (nonatomic) NSURLRequestCachePolicy remoteURLCachePolicy;
+@property (nonatomic) NSTimeInterval remoteURLTimeoutInterval;
+
+/// Creates and stores a web view, and immediately begins loading content in the view as specified by the `webViewStep`. Replaces any previously stored web view.
+/// - Parameters:
+///   - webViewStep: Defines the content to load in the web view.
+///   - key: A unique identifier for the web view.
+- (void)preload:(ORKWebViewStep *)webViewStep forKey:(NSString *)key;
+
 @end
 
 /**
