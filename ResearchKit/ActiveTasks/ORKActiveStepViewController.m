1094d2a361b175cf4e85604c1cbb74f7f0cfb7ef
Eric Schramm <eschramm@mac.com>
2025-06-24 11:54:11 -0500

    PR fix - move NSLog item before the wait block

--- ResearchKit/ActiveTasks/ORKActiveStepViewController.m
+++ ResearchKit/ActiveTasks/ORKActiveStepViewController.m
@@ -489,8 +489,8 @@
             [self stepDidFinish];
         } else {
             _recordersCheckLoopCount += 1;
+            NSLog(@"Waiting for [%lu] recorders to complete...", [_recordersRunning count]);
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                NSLog(@"Waiting for [%lu] recorders to complete...", [_recordersRunning count]);
                 [self goForwardOnceRecordersHaveCompleted:stepDidFinishOnly];
             });
         }
